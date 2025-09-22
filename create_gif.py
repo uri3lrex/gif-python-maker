d4a2adfcabe9ffa5ce6ee4e44207c2684670f02e
uri3lrex <khanhe@tcd.ie>
2025-09-22 09:08:01 +0100

    made the gif

--- create_gif.py
+++ create_gif.py
@@ -1,2 +1,15 @@
 import imageio.v3 as iio
+# adding the files
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = []
+
+# adding a loop to read the images using imageio
+# imread loads image based on file path
+# images has the pictures
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop= 0)
 
